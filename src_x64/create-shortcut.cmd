13636694441a6c10b659b1bb47ad0699dffda3d6
nooriro <noorirogit@gmail.com>
2024-05-04 17:00:00 +0900

    Add 'create/delete shortcut' script
    create-shortcut.cmd - Create shortcut in the Start menu
    delete-shortcut.cmd - Delete shortcut from the Start menu
         shell_link.ps1 - Common dependency

---
+++ src_x64/create-shortcut.cmd
@@ -0,0 +1,6 @@
+@setlocal
+@REM Set %PSModulePath% to default
+@REM : Inspired by 2nd method of https://stackoverflow.com/a/53464542
+@set "PSModulePath=%ProgramFiles%\WindowsPowerShell\Modules;%SystemRoot%\system32\WindowsPowerShell\v1.0\Modules"
+@powershell -ex bypass -f "%~dp0shell_link.ps1"
+@endlocal
